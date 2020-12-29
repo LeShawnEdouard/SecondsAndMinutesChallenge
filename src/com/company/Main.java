880ff98b6c4ec217fe8b9affae8b734b3b86ae39
LeShawn Edouard <leshawnmedouard@gmail.com>
2020-12-29 11:18:07 -0500

    Created double rectangle variable and assigned to the operation to calculate the area of a rectangle.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println(getDurationString(65, 45));
         System.out.println(getDurationString(3945));
         System.out.println(area(5.0));
+        System.out.println(area(5.0, 4.0));
     }
     // Create a method called getDurationString with two parameters, first parameter
     // minutes and 2nd parameter seconds.
@@ -86,12 +87,13 @@ public class Main {
         return circle;
     }
 
-//    public static double area(double x, double y) {
-//        if((x < 0) || (y < 0)) {
-//            return -1.0;
-//        }
-//
-//    }
+    public static double area(double x, double y) {
+        if((x < 0) || (y < 0)) {
+            return -1.0;
+        }
+        double rectangle = x * y;
+        return rectangle;
+    }
 
 
 
